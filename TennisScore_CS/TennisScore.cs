ea04fd5aa074b643b78c1e0d3f1befccf6307da4
alexanderwjrussell <awjrussell@live.co.uk>
2016-12-18 19:07:10 +0000

    Initial Project Setup 1) Set up the project framework and testing

---
+++ TennisScore_CS/TennisScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TennisScore_CS
+{
+	class TennisScore
+	{
+		public string Get(string score)
+		{
+			return null;
+		}
+	}
+}
---
+++ TennisScore_CS/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+namespace TennisScore_CS
+{
+	[TestFixture]
+	public class TennisScoreTest
+	{
+		[Test]
+		public void Should_ReturnLove_When0()
+		{
+			var score = new TennisScore();
+			Assert.That(score.Get("0 - 0"), Is.EqualTo("Love - Love"));
+		}
+	}
+}
