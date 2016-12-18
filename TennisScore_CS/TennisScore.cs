8bb2d02724a98548d377a2128e7f12fecf01a303
alexanderwjrussell <awjrussell@live.co.uk>
2016-12-18 19:39:29 +0000

    Refactored 1) Refactored tests to use test cases 2) Removed any defunkt files

--- TennisScore_CS/TennisScore.cs
+++ TennisScore_CS/TennisScore.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace TennisScore_CS
 {
-	class TennisScore
+	public static class TennisScore
 	{
-		Dictionary<string, string> scores = new Dictionary<string, string>
+		public static Dictionary<string, string> scores = new Dictionary<string, string>
 		{
 			{"0", "Love"},
 			{"15", "15"},
@@ -13,7 +12,7 @@ namespace TennisScore_CS
 			{"40", "40"}
 		};
 
-		public string Get(string score1, string score2)
+		public static string Get(string score1, string score2)
 		{
 			var score = $"{scores[score1]} - {scores[score2]}";
 			return score.Replace("40 - 40", "Deuce");
--- TennisScore_CS/Test.cs
+++ TennisScore_CS/Test.cs
@@ -1,36 +1,18 @@
 ﻿using NUnit.Framework;
-using System;
+
 namespace TennisScore_CS
 {
 	[TestFixture]
 	public class TennisScoreTest
 	{
-		[Test]
-		public void Should_ReturnLove_When0()
+		[TestCase("0", "0", "Love - Love")]
+		[TestCase("15", "15", "15 - 15")]
+		[TestCase("30", "30", "30 - 30")]
+		[TestCase("40", "40", "Deuce")]
+	
+		public void Should_ReturnCorrectScore(string score1, string score2, string result)
 		{
-			var score = new TennisScore();
-			Assert.That(score.Get("0", "0"), Is.EqualTo("Love - Love"));
-		}
-
-		[Test]
-		public void Should_ReturnNormalScore_When15to15()
-		{
-			var score = new TennisScore();
-			Assert.That(score.Get("15", "15"), Is.EqualTo("15 - 15"));
-		}
-
-		[Test]
-		public void Should_ReturnDeuce_When40All()
-		{
-			var score = new TennisScore();
-			Assert.That(score.Get("40", "40"), Is.EqualTo("Deuce"));
-		}
-
-		[Test]
-		public void Should_ReturnEdgeCases_When30to30()
-		{
-			var score = new TennisScore();
-			Assert.That(score.Get("30", "30"), Is.EqualTo("30 - 30"));
+			Assert.That(TennisScore.Get(score1, score2), Is.EqualTo(result));
 		}
 	}
 }
